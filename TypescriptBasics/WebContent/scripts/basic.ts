503533cb3c91ddcdd2c39d51ee5235dc596f7cf8
Murugan_Nagarajan <Murugan_Nagarajan@CHNMCT105003L.ad.infosys.com>
2016-10-24 02:44:49 +0800

    Created a basic static web project with some simple ts & index files. If node, npm, typescript & lite-server are configured correctly you should be able to simply clone this project and see the index.html in your system default browser once you start the lite server.

---
+++ TypescriptBasics/WebContent/scripts/basic.ts
@@ -0,0 +1,11 @@
+function showMsg() {    
+    document.getElementById("msgId").innerHTML = "Index file return this text to try execution of TS/JS snippet in lite-server";
+};
+
+//You can try ScratchJs extension in Google Chrome or Typescript Playground 
+//to see the ES5 equivalent code(JS) transpiled(Typescript is a Transpiler) 
+//from any ES6 code.
+
+//Future, most of the browsers are supposed to support ES6 standards/specifications
+
+
---
+++ TypescriptBasics/WebContent/scripts/timer.js
@@ -0,0 +1,4 @@
+function ShowTime(toDayDate) {
+    document.getElementById("h2Msg").innerHTML = "Time is -- " + toDayDate;
+}
+//# sourceMappingURL=timer.js.map
